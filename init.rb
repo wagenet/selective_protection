db501e2cb7ae6be65b4f0a3017cce382cb4968e9
Peter Wagenet <peter.wagenet@gmail.com>
2009-05-07 15:01:19 -0400

    Added support for Rails 2.1

--- init.rb
+++ init.rb
@@ -1,4 +1,8 @@
-require 'proxy_block'
+if Rails.version >= '2.1' && Rails.version < '3'
+  require 'proxy_block'
 
-ActiveRecord::Base.send :include, SelectiveProtection::WithAccessible
-ActiveRecord::Associations::AssociationProxy.send :include, SelectiveProtection::AssociationHelpers
+  ActiveRecord::Base.send :include, SelectiveProtection::WithAccessible
+  ActiveRecord::Associations::AssociationProxy.send :include, SelectiveProtection::AssociationHelpers
+else
+  puts "selective_protection requires Rails >= 2.1 and < 3"
+end
--- lib/selective_protection/with_accessible.rb
+++ lib/selective_protection/with_accessible.rb
@@ -15,18 +15,21 @@ module SelectiveProtection
         orig_protected_attributes = protected_attributes
         orig_accessible_attributes = accessible_attributes
 
+        protected_name, accessible_name = Rails.version.starts_with?('2.1') ?
+          ['attr_protected', 'attr_accessible'] : [:attr_protected, :attr_accessible]
+
         if !protected_attributes.nil?
           new_columns = allow_all ? [] : (orig_protected_attributes - attrs.map(&:to_s))
-          write_inheritable_attribute(:attr_protected, new_columns)
+          write_inheritable_attribute(protected_name, new_columns)
         elsif !accessible_attributes.nil?
           new_columns = allow_all ? column_names : (orig_accessible_attributes | attrs.map(&:to_s))
-          write_inheritable_attribute(:attr_accessible, new_columns)
+          write_inheritable_attribute(accessible_name, new_columns)
         end
 
         yield
       ensure
-        write_inheritable_attribute(:attr_protected, orig_protected_attributes)
-        write_inheritable_attribute(:attr_accessible, orig_accessible_attributes)
+        write_inheritable_attribute(protected_name, orig_protected_attributes)
+        write_inheritable_attribute(accessible_name, orig_accessible_attributes)
       end
 
     end
